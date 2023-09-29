5d0459e4cba306626265ba97f9998435af8db353
Tyler Jones <tyler@tylerjones.io>
2023-09-29 16:56:11 -0600

    Fix tvOS indexing

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/tylerjonesio/WebRTC/releases/download/117.0.0.b1/WebRTC-2023-09-29T22-33-24.xcframework.zip",
-            checksum: "5d832f162a0a3159e78e047862b95463c547338a5683754109e6dec5af0e3e42"
+            url: "https://github.com/tylerjonesio/WebRTC/releases/download/117.0.0.b2/WebRTC-2023-09-29T22-53-51.xcframework.zip",
+            checksum: "3bc1a7a6b8301f14e81e57751d331a816c77d3e98855ab902ac714258ea00dcc"
         ),
     ]
 )

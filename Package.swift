70dfb220b8741106cd9e4f50631c09beec607b71
Tyler Jones <tyler@tylerjones.io>
2025-06-30 13:48:34 -0600

    Update package hash

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/tylerjonesio/WebRTC/releases/download/133.0.0.b1/WebRTC-2025-02-23T01-26-30.xcframework.zip",
-            checksum: "c65875394aadcf5183350d908284aa641e870ebe4da6d95d5e01d6d3a05fd311"
+            url: "https://github.com/tylerjonesio/WebRTC/releases/download/137.0.0/WebRTC-2025-06-30T18-26-06.xcframework.zip",
+            checksum: "c1cc85be01a87b4cc476a5212eafa6aca916b5dc40047f5ec289d0fe803bd89c"
         ),
     ]
 )
